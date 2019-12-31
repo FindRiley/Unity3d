4a4ac8f2f45e34a4c4bee40291603917a83e5a41
LeoBarwoo <2055718105@qq.com>
2019-12-31 10:00:02 +0800

    h10

---
+++ 10-AR/Assets/Scripts/VRButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+[System.Obsolete]
+public class VRButton : MonoBehaviour, IVirtualButtonEventHandler {
+    
+    public GameObject ani;
+    VirtualButtonBehaviour[] vbbs;
+
+    void Start() {
+        vbbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbbs.Length; i++) {
+            vbbs[i].RegisterEventHandler(this);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour myButton) {
+        switch (myButton.VirtualButtonName) {
+            case "walk":
+                ani.GetComponent<Animator>().SetBool("jump", false);
+                ani.GetComponent<Animator>().SetBool("eat", false);
+                ani.GetComponent<Animator>().SetBool("walk", true);
+                Debug.Log("walk");
+                break;
+            case "jump":
+                ani.GetComponent<Animator>().SetBool("walk", false);
+                ani.GetComponent<Animator>().SetBool("eat", false);
+                ani.GetComponent<Animator>().SetBool("jump", true);
+                Debug.Log("jump");
+                break;
+            case "eat":
+                ani.GetComponent<Animator>().SetBool("jump", false);
+                ani.GetComponent<Animator>().SetBool("walk", false);
+                ani.GetComponent<Animator>().SetBool("eat", true);
+                Debug.Log("eat");
+                break;
+            case "idle":
+                ani.GetComponent<Animator>().SetBool("walk", false);
+                ani.GetComponent<Animator>().SetBool("jump", false);
+                ani.GetComponent<Animator>().SetBool("eat", false);
+                Debug.Log("idle");
+                break;
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour myButton) {
+        
+    }
+    
+
+    void Update() {
+
+    }
+}
